1c2c5b71b1be87c4cff33d434afc520d423645a3
Rafael Volkov <volkov.rafman@mail.ru>
2025-02-23 18:00:55 +0400

    CRO-5 Настроить видимость Header

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -10,11 +10,14 @@ import CreateLobby from "./features/hub/CreateLobby.tsx";
 
 export default function App() {
   const authorizedRoutes = (
-    <Routes>
-      <Route path="*" element={<Navigate to="/home" />} />
-      <Route path="/home" element={<Home />} />
-      <Route path="/createLobby" element={<CreateLobby />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="*" element={<Navigate to="/home" />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/createLobby" element={<CreateLobby />} />
+      </Routes>
+      <Header />
+    </>
   );
 
   const unauthorizedRoutes = (
@@ -30,10 +33,7 @@ export default function App() {
   return (
     <Provider store={store}>
       <BrowserRouter>
-        <div className="max-sm:mt-14 mt-20">
-          {routes}
-          <Header />
-        </div>
+        <div className="max-sm:mt-14 mt-20">{routes}</div>
       </BrowserRouter>
     </Provider>
   );
